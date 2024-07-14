f04e1298affdd830534455acb82e09751ec18c55
carlosouzavalle <carlosouzavalle@gmail.com>
2024-07-13 23:42:16 -0300

    splice method

--- JAVASCRIPT/arrays/metodos.js
+++ JAVASCRIPT/arrays/metodos.js
@@ -13,6 +13,10 @@
     The unshift() method of Array instances adds 
     the specified elements to the beginning of an array and returns the new length of the array.
 
+
+    splice The splice() method of Array instances changes the contents of an array by removing 
+    or replacing existing elements and/or adding new elements in place.
+
 */
 
 
@@ -42,3 +46,19 @@ console.log(nomes)
 //========UNSHIFT - add a element in the first position of the array
 nomes.unshift('Helena')
 console.log(nomes)
+
+
+
+
+let nomes2 = ['Carlos', 'Maria', 'Bruna', 'Camila']
+
+nomes2.splice(2, 1) // no indice 2, remove 1 elemento
+console.log(nomes2)
+
+
+nomes2.splice(2, 1, 'jesica')
+console.log(nomes2)
+
+
+nomes2.splice(2, 0, 'jefiner, samantha')
+console.log(nomes2)
