7032b01afabeb62eb5aa3dd6faafd58600a1cac1
carlosouzavalle <carlosouzavalle@gmail.com>
2024-07-16 23:39:38 -0300

    exercicios de js

---
+++ Playground Javacript/10/main.js
@@ -0,0 +1,37 @@
+// // HERO
+// const HERO = {
+//     Player: 'Hero_name',
+//     level: 1,
+//     Stamina: 100,
+//     Dano_player: 150,
+//     Defesa_player: 50,
+// }
+
+
+// //MOBS
+// let monstro_pequeno = 100
+
+// if(monstro_pequeno < HERO.Dano_player) {
+
+//     console.log('Você Matou a Ameaça')
+//     for(let monstro_pequeno; monstro_pequeno < HERO.Dano_player; monstro_pequeno--) {
+
+
+//         console.log(monstro_pequeno)
+
+//         // if(HERO.level++) {
+//         //     console.log(`Você upou para o  ${HERO.level} nivel`)
+//         // }
+
+
+//         // if(HERO.Stamina = 0) {
+//         //     console.log('zeros mobs')
+
+//         //     if(HERO.level += 0) {
+//         //         console.log(`Você upou para o  ${HERO.level} nivel`)
+//         //     }
+//         // }
+
+//     }
+// }
+
---
+++ Playground Javacript/11/main.js
@@ -0,0 +1,8 @@
+/*
+Números Primos
+    Escreva uma função que receba um número e retorne true se ele for primo e false caso contrário.
+*/
+
+let NumeroPrimo = function(numero) {
+
+}
