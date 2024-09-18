7e74d4dea1fc72ba07305aa0511d0c90fbf4201c
carlosouzavalle <carlosouzavalle@gmail.com>
2024-09-18 19:23:42 -0300

    Menu de criação de personagem quase lá

--- Playground Javacript/rpg/game.js
+++ Playground Javacript/rpg/game.js
@@ -12,6 +12,7 @@ let IntroductionHistory = document.createElement('div')
 let History = document.createElement('p')
 let nextButton = document.createElement('button')
 let AtackButton = document.createElement('button')
+let SelectClassButton = document.createElement('button')
 
 
 
@@ -20,7 +21,6 @@ function ChooseClasse() {
     inputName.style.display = 'none';
     startButton.style.display = 'none';
 
-    
 
     let chooseClassDiv = document.createElement('div');
     chooseClassDiv.classList.add('chooseClassDiv');
@@ -49,6 +49,9 @@ function ChooseClasse() {
 
     // Cria um container para a imagem
     let imgContainer = document.createElement('div');
+    
+    imgContainer.classList.add('imgContainer')
+
     chooseClassDiv.appendChild(imgContainer);
 
 
@@ -65,93 +68,106 @@ function ChooseClasse() {
             imgContainer.appendChild(guerreiroIMG);
         } else if (selectMenu.value === 'Mago') {
             let magoIMG = document.createElement('img');
-            magoIMG.src = './img/mago.png'; // Caminho da imagem do mago
-            magoIMG.width = 200;
+            magoIMG.src = './img/172668824662537404.png'; // Caminho da imagem do mago
+            magoIMG.width = 180;
             imgContainer.appendChild(magoIMG);
         } else if (selectMenu.value === 'Arqueiro') {
             let arqueiroIMG = document.createElement('img');
             arqueiroIMG.src = './img/arqueiro.png'; // Caminho da imagem do arqueiro
-            arqueiroIMG.width = 200;
+            arqueiroIMG.width = 130;
             imgContainer.appendChild(arqueiroIMG);
         } else if (selectMenu.value === 'Assassino') {
             let assassinoIMG = document.createElement('img');
-            assassinoIMG.src = './img/assassino.png'; // Caminho da imagem do assassino
-            assassinoIMG.width = 200;
+            assassinoIMG.src = './img/assasino.png'; // Caminho da imagem do assassino
+            assassinoIMG.width = 165;
             imgContainer.appendChild(assassinoIMG);
         }
     });
 
+
+
+    SelectClassButton.textContent = 'Select Class'
+    SelectClassButton.classList.add('SelectClassButton')
+    chooseClassDiv.appendChild(SelectClassButton)
+
+    SelectClassButton.addEventListener('click', function() {
+
+        console.log('clicou')
+        inputName.style.display = 'block';
+        startButton.style.display = 'block';
+        chooseClassDiv.style.display = 'none'
+        selectMenu.style.display = 'none'
+    
+        // loggin e algumas funcionalidades
+    startButton.addEventListener('click', function () {
+        let = Getname = inputName.value
+    
+        //variaveis de estrutura
+        let buttonContinue = document.createElement('button')
+        let contentDiv = document.querySelector('.login')
+    
+    
+        if (Getname) {
+            welcomMensage.textContent = 'Welcome, ' + Getname + ' To Rosalia Word!!'
+            loginImg.style.display = 'block'
+            ErrorloginImg.style.display = 'none'
+            startButton.style.display = 'none'
+            inputName.style.display = 'none'
+    
+    
+            // propriedades do button
+            buttonContinue.style.width = '150px'
+            buttonContinue.style.height = '50px'
+            buttonContinue.style.fontSize = '20px'
+            buttonContinue.style.display = 'block'
+            buttonContinue.textContent = 'Continue'
+            buttonContinue.classList.add('large-button')
+    
+            contentDiv.appendChild(buttonContinue)
+    
+        } else {
+            welcomMensage.textContent = 'Please type your name!'
+            loginImg.style.display = 'none'
+            ErrorloginImg.style.display = 'block'
+        }
+    
+    
+        //iniciando o game
+        buttonContinue.addEventListener('click', function () {
+            console.log('alooo')
+            buttonContinue.style.display = 'none'
+            startButton.style.display = 'none'
+            loginImg.style.display = 'none'
+            welcomMensage.style.display = 'none'
+    
+            //container da historia do game
+            IntroductionHistory.style.width = '100vw'
+            IntroductionHistory.style.height = '100vh'
+            IntroductionHistory.style.backgroundColor = 'bisque'
+            IntroductionHistory.classList.add('IntroductionHistory')
+            contentDiv.appendChild(IntroductionHistory)
+    
+            contentDiv.classList.add('contentDiv')
+            // referente ao paragrafo que esta dentro do container
+            IntroductionHistory.appendChild(History)
+            History.classList.add('History_class')
+            History.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    
+            //button next container da historia
+            nextButton.style.width = '100px'
+            nextButton.style.height = '50px'
+            nextButton.style.fontSize = '20px'
+            nextButton.textContent = 'Next'
+            IntroductionHistory.appendChild(nextButton)
+            nextButton.classList.add('nextButton')
+    
+        })
+    })
+    
+    })
+
 }
 ChooseClasse();
-
-
-
-// loggin e algumas funcionalidades
-startButton.addEventListener('click', function () {
-    let = Getname = inputName.value
-
-    //variaveis de estrutura
-    let buttonContinue = document.createElement('button')
-    let contentDiv = document.querySelector('.login')
-
-
-    if (Getname) {
-        welcomMensage.textContent = 'Welcome, ' + Getname + ' To Rosalia Word!!'
-        loginImg.style.display = 'block'
-        ErrorloginImg.style.display = 'none'
-        startButton.style.display = 'none'
-        inputName.style.display = 'none'
-
-
-        // propriedades do button
-        buttonContinue.style.width = '150px'
-        buttonContinue.style.height = '50px'
-        buttonContinue.style.fontSize = '20px'
-        buttonContinue.style.display = 'block'
-        buttonContinue.textContent = 'Continue'
-        buttonContinue.classList.add('large-button')
-
-        contentDiv.appendChild(buttonContinue)
-
-    } else {
-        welcomMensage.textContent = 'Please type your name!'
-        loginImg.style.display = 'none'
-        ErrorloginImg.style.display = 'block'
-    }
-
-
-    //iniciando o game
-    buttonContinue.addEventListener('click', function () {
-        console.log('alooo')
-        buttonContinue.style.display = 'none'
-        startButton.style.display = 'none'
-        loginImg.style.display = 'none'
-        welcomMensage.style.display = 'none'
-
-        //container da historia do game
-        IntroductionHistory.style.width = '100vw'
-        IntroductionHistory.style.height = '100vh'
-        IntroductionHistory.style.backgroundColor = 'bisque'
-        IntroductionHistory.classList.add('IntroductionHistory')
-        contentDiv.appendChild(IntroductionHistory)
-
-        contentDiv.classList.add('contentDiv')
-        // referente ao paragrafo que esta dentro do container
-        IntroductionHistory.appendChild(History)
-        History.classList.add('History_class')
-        History.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-
-        //button next container da historia
-        nextButton.style.width = '100px'
-        nextButton.style.height = '50px'
-        nextButton.style.fontSize = '20px'
-        nextButton.textContent = 'Next'
-        IntroductionHistory.appendChild(nextButton)
-        nextButton.classList.add('nextButton')
-
-    })
-})
-
 
 
 
@@ -198,16 +214,15 @@ nextButton.addEventListener('click', function () {
     divStats.appendChild(cardAtack)
     divStats.appendChild(carDef)
 
-    // Buttons
-    IntroductionHistory.appendChild(AtackButton)
-    AtackButton.textContent = 'ATACAR'
-    AtackButton.classList.add('AtackButton')
-    IntroductionHistory.appendChild(nextButton);
+    // // Buttons
+    // IntroductionHistory.appendChild(AtackButton)
+    // AtackButton.textContent = 'ATACAR'
+    // AtackButton.classList.add('AtackButton')
+    // IntroductionHistory.appendChild(nextButton);
 
 });
 
 
-
-AtackButton.addEventListener('click', function() {
+// AtackButton.addEventListener('click', function() {
     
-})
+// })
