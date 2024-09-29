f29349a1853eb57847f61260a3edecb12f20dc61
carlosouzavalle <carlosouzavalle@gmail.com>
2024-09-28 22:01:13 -0300

    next

--- React/estudandoreact/src/app/components/Card.tsx
+++ React/estudandoreact/src/app/components/Card.tsx
@@ -1,15 +1,40 @@
 interface Cardprops {
     produto: string,
     valor: number,
-    desconto: number, 
+    desconto: number,
     funcao: any
 }
 
 export default function Card(props: Cardprops) {
     return (
-        <div className="flex flex-col border border-red-600 rounded-sm p-1">
+        <div className={`flex justify-center flex-col border-4 ${props.desconto > 0 ? 'border-red-600' : 'border-blue-700'}  rounded-sm p-1`}>
             <div>Produto: {props.produto}</div>
-            <div>Valor: R${props.funcao(props.valor, props.desconto)}</div>
-        </div>
+            <div>Valor: {props.valor}</div>
+
+            {/* Renderização condicional */}
+            {/* o que vai acontecer aqui é que ele simplismente vai executar e pronto
+            porem se você quiser apresentar uma mensagem
+            
+            */}
+
+            {props.desconto > 0 && (
+                <div>
+                    <div>Produto: {props.produto}</div>
+                    <div>Valor: R${props.funcao(props.valor, props.desconto)}
+                    </div>
+
+                </div>
+            )}
+
+
+
+
+            {/* {props.desconto > 0 ? (
+            <div>
+                <div>Produto: {props.produto}</div>
+                <div>Valor: R${props.funcao(props.valor, props.desconto)}</div>
+            </div>
+        ): "Sua msg aqui"} */}
+         </div>
     )
 }
--- React/estudandoreact/src/app/components/comp1.tsx
+++ React/estudandoreact/src/app/components/comp1.tsx
@@ -2,6 +2,47 @@ import Card from "./Card"
 
 export default function Comp1() {
 
+    const produtos=[
+        {
+            produto:'Mouse',
+            valor: 49.9,
+            desconto: 0,
+            disponivel: true
+        },
+        {
+            produto:'Teclado',
+            valor: 69.9,
+            desconto: 0,
+            disponivel: true
+        },
+        {
+            produto:'Monitor',
+            valor: 1149.9,
+            desconto: 0,
+            disponivel: true
+        },
+        {
+            produto:'CPU',
+            valor: 749.9,
+            desconto: 200,
+            disponivel: true
+        },
+        {
+            produto:'Caixa de Som',
+            valor: 149.9,
+            desconto: 50,
+            disponivel: true
+        },
+        {
+            produto:'Microfone',
+            valor: 249.9,
+            desconto: 0,
+            disponivel: false
+        }  
+    ]
+
+
+
     function Funcao() {
         return (
             <div className="flex justify-between items-center bg-slate-800 text-white h-[200px]">
@@ -46,9 +87,20 @@ export default function Comp1() {
             </div>
 
             <div className="flex justify-center gap-4">
-                <Card produto={'mouse'} valor={50} desconto={2} funcao={calcDesconto}/>
+                {/* <Card produto={'mouse'} valor={50} desconto={2} funcao={calcDesconto}/>
                 <Card produto={'teclado'} valor={500} desconto={3} funcao={calcDesconto}/>
-                <Card produto={'monitor'} valor={5000} desconto={10} funcao={calcDesconto2}/>
+                <Card produto={'monitor'} valor={5000} desconto={0} funcao={calcDesconto2}/> */}
+
+
+                {
+                    produtos.map((e:any) => {
+                        if(e.disponivel) {
+                            return (
+                                <Card produto={e.produto} valor={e.valor} desconto={e.desconto} funcao={calcDesconto}/>
+                            )
+                        }
+                    })
+                }
             </div>
 
         </div>
