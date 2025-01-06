94317f707990fd23d8fe454c5b09aa449753f4bb
carlosouzavalle <carlosouzavalle@gmail.com>
2025-01-05 23:50:35 -0300

    revisão

--- JAVASCRIPT/arrays/revisando1.js
+++ JAVASCRIPT/arrays/revisando1.js
@@ -7,4 +7,25 @@ console.log(arr)
 
 // mb o animal aqui não explico o que é um array, de forma resumida um array é uma gaveta de roupas onde você pode guarda
 // varias coisas tipo camisa, meias ,calças etc...
+// Temos alguns metodos para manipular um array um deles é o push que vimos logo acima.
 
+arr.pop()
+console.log(arr)
+
+// o metodo pop remove o ultimo elemento
+
+arr.shift()
+console.log(arr)
+
+// o metodo shift a gente remove o primeiro elemento do array
+
+arr.unshift('patricia')
+console.log(arr)
+
+// o metodo unshift add um elemento no começo do array
+
+
+arr.splice(1, 3) 
+console.log(arr)
+
+// o metodo splice ele pode remover ou add elementos 1, 3 apartir do indice 1 remova 3 elementos
